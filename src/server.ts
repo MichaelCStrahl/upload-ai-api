d777ab88189c62737a5939b59a806f308eaa9330
Michael C. Strahl <michaelstrahl.sm@gmail.com>
2023-09-12 23:33:19 -0300

    connected openai API

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from 'fastify'
 import { getAllPrompsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
 
 const app = fastify()
 
 app.register(getAllPrompsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({ port: 3333 }).then(() => {
   console.log(':rocket: HTTP Server Running!')
